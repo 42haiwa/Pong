cf1dd20952a86368943fce5cce952a52c4a80b08
haiwaCase <clement222206@gmail.com>
2021-01-15 21:11:52 +0100

    Ball can bounce

--- include/Ball.h
+++ include/Ball.h
@@ -2,11 +2,16 @@
 #define __BALL__H__
 
 #include <SFML/Graphics.hpp>
+#include <math.h>
 #include "PongEntity.h"
 
 class Ball : public PongEntity {
     private:
         sf::RectangleShape m_rectangle;
+
+        float m_angle{0.f};
+        float m_speedMoveX{500.f};
+        float m_speedMoveY{500.f};
 
         static constexpr float m_size{15.f};
 
@@ -14,6 +19,7 @@ class Ball : public PongEntity {
         Ball(sf::Vector2f const & position);
         void update();
         void render(sf::RenderWindow & renderWindow);
+        void move(sf::Time dt);
 };
 
 #endif
--- src/Ball.cpp
+++ src/Ball.cpp
@@ -3,11 +3,27 @@
 Ball::Ball(sf::Vector2f const & position) : PongEntity{position} {
     m_rectangle = sf::RectangleShape{sf::Vector2f{m_size, m_size}};
     m_rectangle.setPosition(m_position);
+
+    m_angle = 45.f;
 }
 
 void Ball::update() {
+    sf::Time dt = m_clock.restart();
+    move(dt);
 }
 
 void Ball::render(sf::RenderWindow & renderWindow) {
     renderWindow.draw(m_rectangle);
+}
+
+void Ball::move(sf::Time dt) {
+    m_position.x += m_speedMoveX * dt.asSeconds() * (cos((m_angle) * M_PI / 180));
+    m_position.y += m_speedMoveY * dt.asSeconds() * (sin((m_angle) * M_PI / 180));
+
+    if (m_position.x < 0) m_speedMoveX *= -1;
+    if (m_position.x > 800 - m_size) m_speedMoveX *= -1;
+    if (m_position.y < 0) m_speedMoveY *= -1;
+    if (m_position.y > 600 - m_size) m_speedMoveY *= -1;
+
+    m_rectangle.setPosition(m_position);
 }
